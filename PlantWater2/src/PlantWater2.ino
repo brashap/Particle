36f0b235c10fc390a0b16969e98ea982b5e2f2ae
IOT Instructor <brashap@cnm.edu>
2020-12-06 19:24:31 -0700

    Plant2

--- PlantWater2/src/PlantWater2.ino
+++ PlantWater2/src/PlantWater2.ino
@@ -5,7 +5,41 @@
  * Date: 3-DEC-2020
  */
 
+#include "Credentials.h"
+//#include <JsonParserGeneratorRK.h>
+#include <Adafruit_MQTT.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
 
+// Setup SSD_1306 Display
+#define OLED_ADDR   0x3C
+#define SSD1306_128_64
+Adafruit_SSD1306 display(-1);
+
+// THese #include statement is for MQTT
+#include "Adafruit_MQTT/Adafruit_MQTT.h" 
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h" 
+#include "Adafruit_MQTT/Adafruit_MQTT.h" 
+
+/************ Global State (you don't need to change this!) ***   ***************/ 
+TCPClient TheClient; 
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details. 
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
+
+
+/****************************** Feeds ***************************************/ 
+// Setup a feed called 'voltage' for publishing. 
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname> 
+
+Adafruit_MQTT_Publish Htemp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Home_Temperature");
+Adafruit_MQTT_Publish Hmoist = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Home_Moisture");
+Adafruit_MQTT_Publish Hwater = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Home_Water"); 
+Adafruit_MQTT_Publish Hpres = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Home_Pressure"); 
+Adafruit_MQTT_Publish Hhum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Home_Humidity"); 
+Adafruit_MQTT_Publish Hdust = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Home_Dust"); 
+
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/LED_On"); 
 
 // Declare Constants
 const byte BME_ADDR = 0x67;
@@ -13,7 +47,16 @@ const byte OLED_ADDR = 0x3C;
 const byte BLUEPIN = D9;
 const byte YELLOWPIN = D11;
 const byte RELAYPIN = D10;
+const byte MOISTPIN = A4;
+const byte AQPIN = A0;
+const byte DUSTPIN = A1;
 
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme; 
+
+/************Declare Variables*************/
+int moist;
+int soilDelay = 60000;
 
 void setup() {
   Serial.begin(9600);
@@ -23,6 +66,8 @@ void setup() {
   pinMode(BLUEPIN,INPUT_PULLDOWN);
   pinMode(YELLOWPIN,INPUT_PULLDOWN);
   pinMode(RELAYPIN,OUTPUT);
+  pinMode(MOISTPIN,INPUT);
+  pinMode(DUSTPIN,INPUT);
 }
 
 
